3666bb08ee5f4412777dcfb156fbf1d12bc4315b
AirZED <regnalukpabio@gmail.com>
2024-03-17 16:32:36 +0100

    image upload successful

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -4,42 +4,13 @@ import catchAsync from '../utils/catchAsync';
 import AppError from '../utils/appError';
 import sendReponse from '../utils/sendResponse';
 import User from '../models/userModel';
-import multer from 'multer';
 import filterObj, { keysToExtract } from '../utils/filterObj';
 
 class UserController {
-  multerStorage = multer.diskStorage({
-    destination(req, file, callback) {
-      callback(null, 'public/img/users');
-    },
-    filename(req: CustomRequest, file, callback) {
-      const extension = file.mimetype.split('/')[1];
-      callback(null, `user-${req.user?.id}-${Date.now()}.${extension}`);
-    },
-  });
-
-  multerFilter = (
-    req: CustomRequest,
-    file: Express.Multer.File,
-    callback: multer.FileFilterCallback,
-  ) => {
-    if (file.mimetype.startsWith('image')) {
-      callback(null, true);
-    } else {
-      callback(new AppError('Not an image! Please upload only images', 400));
-    }
-  };
-
-  // initializing multer
-  upload: multer.Multer = multer({
-    storage: this.multerStorage,
-    fileFilter: this.multerFilter,
-  });
-
   // constructor() {}
   updateProfile: RequestHandler = catchAsync(
     async (req: CustomRequest, res, next) => {
-      const user_id = req.user?.id;
+      const id = req.user?.id;
 
       if (req.body.password || req.body.passwordConfirm) {
         return next(
@@ -49,10 +20,10 @@ class UserController {
 
       // filter body properly
       const filterBody = filterObj(req.body, keysToExtract);
-      if (req.file) filterBody.photo = req.file.filename;
+      if (req.file) filterBody.image = req.file.filename;
 
       const profile = await User.findOneAndUpdate(
-        { id: user_id },
+        { _id: id },
         { $set: filterBody },
       );
 
@@ -67,7 +38,7 @@ class UserController {
     async (req: CustomRequest, res, next) => {
       const { user_id } = req.params;
 
-      const profile = await User.findOne({ id: user_id });
+      const profile = await User.findOne({ _id: user_id });
 
       if (!profile) {
         return next(new AppError('An error occured fetching userProfile', 400));
@@ -90,8 +61,6 @@ class UserController {
 
     sendReponse(res, 200, users);
   });
-
-  uploadProfileImage = this.upload.single('photo');
 }
 
 export default new UserController();
--- src/routes/userProfileRoutes.ts
+++ src/routes/userProfileRoutes.ts
@@ -1,23 +1,51 @@
 import { Router } from 'express';
 import userController from '../controllers/userController';
 import authController from '../controllers/authController';
+import multer from 'multer';
+import { CustomRequest } from '../controllers/authController';
+import AppError from '../utils/appError';
 
 const router = Router();
+
+const multerStorage = multer.diskStorage({
+  destination(req: CustomRequest, file, callback) {
+    callback(null, 'public/img/users');
+  },
+  filename(req: CustomRequest, file, callback) {
+    const extension = file.mimetype.split('/')[1];
+    callback(null, `user-${req.user?.id}-${Date.now()}.${extension}`);
+  },
+});
+
+const multerFilter = (
+  req: CustomRequest,
+  file: Express.Multer.File,
+  callback: multer.FileFilterCallback,
+) => {
+  if (file.mimetype.startsWith('image')) {
+    callback(null, true);
+  } else {
+    callback(new AppError('Not an image! Please upload only images', 400));
+  }
+};
+
+// initializing multer
+const upload: multer.Multer = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
 
 router
   .route('/')
   .get(userController.getUsers)
   .patch(userController.updateProfile);
 
-router
-  .route('/:user_id')
-  .get(userController.getProfile)
-  .patch(userController.uploadProfileImage);
+router.route('/:user_id').get(userController.getProfile);
 
 router.patch(
   '/image',
   authController.protect,
-  userController.uploadProfileImage,
+  upload.single('photo'),
   userController.updateProfile,
 );
 
